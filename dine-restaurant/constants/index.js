b48e7ec4ec8924731810282fa38a23c0a43e1472
Jake Samuels <samuels242@googlemail.com>
2023-10-20 20:48:39 +0100

    About section text

---
+++ dine-restaurant/constants/index.js
@@ -0,0 +1,12 @@
+export const aboutText = [
+  {
+    image: "/assets/homepage/enjoyable-place-desktop@2x.jpg",
+    heading: "Enjoyable place for all the family",
+    text: "Our relaxed surroundings make dining with us a great experience for everyone. We can even arrange a tour of the farm before your meal.",
+  },
+  {
+    image: "/assets/homepage/locally-sourced-desktop@2x.jpg",
+    heading: "The most locally sourced food",
+    text: "All our ingredients come directly from our farm or local fishery. So you can be sure that you’re eating the freshest, most sustainable food.",
+  },
+];
